b7f516ce6010810a3dc5b6f70cb2244dc58e026e
sholomka <sholomka@gmail.com>
2017-02-24 18:43:12 +0200

    add

--- js/carousel.js
+++ js/carousel.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $("#owl-example").owlCarousel({
         navigation : true,
         items : 3,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
+        margin: 20,
+        loop: true
     });
 });/**
  * Created by skyey on 22.02.2017.
